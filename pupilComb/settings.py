7ac44252edf205454913ee43c4bc6918075c0e14
Jan Cas <jan.l.cas@outlook.com>
2021-11-30 10:52:33 -0700

    postgres db

--- pupilComb/settings.py
+++ pupilComb/settings.py
@@ -76,11 +76,25 @@ WSGI_APPLICATION = 'pupilComb.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'local',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'admin',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
+
 
 
 # Password validation
