55c2c05c08c6eeda201df699555bc4a0e4f83984
Jan Cas <jan.l.cas@outlook.com>
2022-01-06 19:12:52 -0800

    bug and celery fixes

--- apps/base/models/SemesterCourseRecordingItem.py
+++ apps/base/models/SemesterCourseRecordingItem.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.utils import timezone
-
 class SemesterCourseRecordingItem(models.Model):
 
     semester_course = models.ForeignKey('base.SemesterCourse', on_delete=models.CASCADE)
